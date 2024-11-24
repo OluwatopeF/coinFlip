171e5e35a84e66c0be09b67520a84857f3cb4752
OluwatopeF <topeumar@gmail.com>
2024-11-24 12:32:44 -0500

    update 11/24/2024

--- react-coinFlip/src/App.tsx
+++ react-coinFlip/src/App.tsx
@@ -1,11 +1,9 @@
 //
 import FlipOdds from './components/FilpOdds';
-import Coin from './components/Coin';
 
 function App(){
   return (
     <div className='mainApp'>
-      <Coin/>
       <FlipOdds/>
     </div>
   );
--- react-coinFlip/src/components/FilpOdds.tsx
+++ react-coinFlip/src/components/FilpOdds.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import '../App.css';
 
 // an algotithm that chooses a random number for heads(odd) or tails(even)
 function FlipOdds() {
@@ -9,30 +10,57 @@ function FlipOdds() {
     const [headCounter, setHeadCounter] = useState(0);
     const [tailCounter, setTailCounter] = useState(0);
     
+    const[coin, setCoin] = useState(0);
+    
     const handleClick = () => {
-        setnumberGen(Math.floor(Math.random() * 100) + 1);
+
+        setCoin(1);
+
+        setTimeout(() =>{
+
+            setnumberGen(Math.floor(Math.random() * 100) + 1);
+
+            if(numberGen % 2 === 0 ){
+                setHeadCounter((prev) => prev +1);
+
+            } else{
+                setTailCounter((prev) => prev +1);
+            }
+
+            setCoin(0);
+        }, 1000)
         
-        if(numberGen % 2 === 0 ){
-            setHeadCounter((prev) => prev +1);
-        } else{
-            setTailCounter((prev) => prev +1);
-        }
-        
-    }
+    };
+
     
 
     // Result
         return (
             <>
-                <div className="coin-stat">
-                <button onClick={handleClick}>Flip Coin</button> <br></br>
-                
-                {/* result from numberGen needs to stay invisible during flip animation*/}
-                {numberGen % 2 != 0 && <h1>You flipped Heads </h1>}
-                {numberGen % 2 == 0 && <h1>You flipped Tails</h1>}
+                <div className="appContainer">
+                    <div 
+                    className={coin ? 'coin animating': 'coin' }
+                    onClick={handleClick}>
+                        <div className="heads">
+                            {/* replace with images */}
+                            <h1>HEADS</h1>
+                        </div>
 
-                { <h1>Heads: {headCounter} Time(s)</h1>}
-                {<h1>Tails: {tailCounter} Time(s)</h1>}
+                        <div className="tails">
+                            <h1>TAILS</h1>
+                        </div>
+                    </div>
+
+                    <div className="coin-stat">
+                    <button onClick={handleClick}>Flip Coin</button> <br></br>
+                    
+                    {/* result from numberGen needs to stay invisible during flip animation*/}
+                    {numberGen % 2 != 0 && <h1>You flipped Heads </h1>}
+                    {numberGen % 2 == 0 && <h1>You flipped Tails</h1>}
+
+                    { <h1>Heads: {headCounter} Time(s)</h1>}
+                    {<h1>Tails: {tailCounter} Time(s)</h1>}
+                    </div>
                 </div>
             </>
         );
